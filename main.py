27105c4ff1fb1c520fcf5c8883950192e11c3164
BlackCage <66648711+BlackCage@users.noreply.github.com>
2023-11-09 15:43:14 +0100

    v0.2

--- main.py
+++ main.py
@@ -6,46 +6,45 @@ from proxy_information import ProxyInformation
 # Create an instance of the ProxyInformation class to check proxies
 checker = ProxyInformation(timeout=2)
 
-# Function to write a proxy to a file based on its type
-def write_to_file(proxy, proxy_type):
-    if proxy:
-        # Verifica si la línea es un proxy IP:puerto válido antes de escribirla
-        if re.match(r'\d+\.\d+\.\d+\.\d+:\d+', proxy):
-            with open(f"./Proxies/{proxy_type}.txt", "a") as file:
-                file.write(proxy + "\n")
+def write_proxy_to_file(proxy_info):
+    """
+    Write a proxy and its protocol to a corresponding file.
 
-# Function to check a group of proxies
-def check_proxies(proxy_group):
-    for proxy in proxy_group:
-        result = checker.check_proxy(proxy)
-        # Only write the proxy to a file if it's valid (result is not "False")
-        if not "False" in str(result):
-            proxy = f"{result['ip']}:{result['port']}"
-            proxy_type = result['protocol']
-            write_to_file(proxy, proxy_type)
+    Args:
+        proxy_info (dict): Dictionary containing proxy and protocol information.
+    """
+    proxy = proxy_info["proxy"]
+    protocol = proxy_info["protocol"]
+    
+    with open(f"./Proxies/{protocol}.txt", "a") as file:
+        file.write(proxy + "\n")
+
+def check_proxy_and_write(proxy):
+    """
+    Check a proxy using the ProxyInformation class and write it to a file if valid.
+
+    Args:
+        proxy (str): Proxy in the format "ip:port".
+    """
+    result = checker.check_proxy(proxy)
+    if result["status"]:
+        proxy_type = result['info']['protocol']
+        write_proxy_to_file({"proxy": proxy, "protocol": proxy_type})
 
 def main():
     # Use the ProxyScraper to scrape and clean proxies
     scraper = ProxyScraper()
     scraper.run_scraper()
-    
+
     with open("./Proxies/Not_Processed/proxies.txt", "r", encoding="utf-8") as file:
         # Read the list of proxies from the file and remove leading/trailing whitespace
         proxies = [line.strip() for line in file]
 
-    num_workers = 200
-    chunk_size = len(proxies) // num_workers
+    num_workers = 100
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-        futures = []
-        for i in range(0, len(proxies), chunk_size):
-            proxy_group = proxies[i:i + chunk_size]
-            # Submit the check_proxies function for parallel execution
-            future = executor.submit(check_proxies, proxy_group)
-            futures.append(future)
-
-        # Wait for all futures to complete
-        concurrent.futures.wait(futures, return_when=concurrent.futures.ALL_COMPLETED)
+        # Map the check_proxy_and_write function using the list of proxies
+        executor.map(check_proxy_and_write, proxies)
 
 if __name__ == "__main__":
     main()
